af05ccdacb04001cd6cf30e7e5567465aee76e65
Chushmitha <chushmithabattula99@gmail.com>
2025-11-10 13:33:20 -0700

    modifying the load data and adding the test suite first check

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-Q1. Build a Table of Contents (Tree)
+# Data-Structures-and-Algorithms-Group-Project-2
+# About Project
+**Group No: 8**
 
+**Group Members: Andrea Caceres, Sam Goodell, Chushmitha Battula, Samanwita Mukherjee**
 
-Q2.  Letters & Words Analysis (No NLP Libraries)
+**Project Name: Data Structures in Action Table of Contents & Text Analysis**
+
+**Github Repo link**:  https://github.com/organiccaprisunn/Data-Structures-and-Algorithms-Group-Project-2/tree/main
+
+**Objective**: Analyze large text datasets using efficient counting and aggregation strategies.
+
+# Team roles:
+
+This project consists of two questions. Question 1 consists of creating a hierarchical Table of Contents from Data Science and Predictive Analytics by Ivo D. Dinov. Question 2 consists of conducting an analysis of Wuthering Heights by Emily Bronte by preprocessing the text and implementing different text analysis. Andrea Caceres and Samanwita finished the code by working individually then during a meeting, combining their code together. 
+
+Sam Goodell navigated planning zoom calls by finding times that worked for the group. Andrea Caceres created a group chat with everyone in order to easily discuss logistics. 
--- src/toc_tree.py
+++ src/toc_tree.py
@@ -37,56 +37,29 @@ class Node:
         for val in arr:
             self.insert_section(val[0], val[1], val[2])
 
-
-    def display_TOC(self, book):
-        if book is None:
-            return
-        queue = []
-        queue.append(book)
-        while len(queue) > 0:
-            print(queue[0].book, queue[0].section, queue[0].title, queue[0].page)
-            cur_section = queue.pop(0)
-            for element in cur_section.children:
-                queue.append(element)
-
     def print_toc(self, mode):
       # will store the values in list instead of printing them all
         lines = []
+
         def traverse(node, chapter_hierarchy, level):
-          # wit
             if node.title:
-              # if the node has an existing not none title, we concatenate the hierarchy to use it later in the numbered indentation
                 number_str = ".".join(str(num) for num in chapter_hierarchy)
-              # level helps with keeps track of how many indent positions should we apply based on the level in TOC
                 indent = "  " * level
                 if mode == "plain":
-              # we just print the book TOC titles, when asked for plain formatting 
                     lines.append(node.title)
                 elif mode == "indented":
-              # we use the pre-calculated indent variable and concatenate it to the title, when its asked to print with indentation format
                     lines.append(indent + node.title)
-              # to print with number and indentation we use the above 2 pre-calculated variables to display the contents as requried 
                 elif mode == "numbered":
                     lines.append(indent + (f"{number_str} " if number_str else "") + node.title)
-           
-           # we loop over the children [], and perform preorder traversal here, to print the current node first and look into its children node, to maintain the TOC structure 
 
             for i, child in enumerate(node.children, 1):
-
                 traverse(child, chapter_hierarchy + [i], level + 1)
 
-        # initial function call, that will execute the function
-
         traverse(self, [], 0)
-
-        # returns the list of lines we required from the TOC, which will be used in the show_toc() method to display it correctly
-
         return lines
 
     def show_toc(book, mode):
-      # method used to print the TOC in the colab cells without any interrupted
         toc_lines = book.print_toc(mode)
-      # adds the line break after each line of the content to display it in html content
         html_block = "<br>".join(toc_lines)
         display(HTML(f"""<div style="max-height: 400px; overflow-y: auto; white-space: pre; font-family: monospace;">{html_block}</div>"""))
 
@@ -105,23 +78,19 @@ class Node:
         #base case - return 0 because root node has height 0
         else:
             return 0
-        # return max height + 1 to increment each recurion
+        #return max height + 1 to increment each recurion
         return 1 + max(heights_arr)
 
     def tree_depth(self, title):
-        # using dfs approach here as we want to have the minimal traversal made to identify the actual title 
-        # we start with 0 at the root and the depth is 0 at root
+        #using dfs approach here
         if self.book == title:
           return 0
         def dfs(node, depth):
-          # check with title and return the depth when founded
             if node.title == title:
                 return depth
             for child in node.children:
-          # we loop through all the children [] and increase the depth by 1, with each recursive call
                 result = dfs(child, depth + 1)
                 if result is not None:
                     return result
             return None
-        # this will call our dfs method
         return dfs(self, 0)
